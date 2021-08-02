ccf5b0a692a1dc7060ffac21beb7693c137b331e
clairenjit369 <85318380+clairenjit369@users.noreply.github.com>
2021-08-02 02:53:15 -0400

    update

--- project04/project04/app/app.py
+++ project04/project04/app/app.py
@@ -16,12 +16,8 @@ mysql.init_app(app)
 @app.route('/', methods=['GET'])
 def index():
     return "Basic Test"
-    # user = {'username': 'MLB Players Project'}
-    # cursor = mysql.get_db().cursor()
-    # cursor.execute('SELECT * FROM tblMlbPlayersImport')
-    # result = cursor.fetchall()
-    # return render_template('index.html', title='Home', user=user, players=result)
+
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8081, debug=True)
