1bc7249c087dd3e009972a5e2f492e4f8fc10133
clairenjit369 <85318380+clairenjit369@users.noreply.github.com>
2021-08-02 01:59:47 -0400

    update

--- flask-sqlalchemy-tutorial/flask_sqlalchemy_tutorial/routes.py
+++ flask-sqlalchemy-tutorial/flask_sqlalchemy_tutorial/routes.py
@@ -1,21 +1,63 @@
-"""Initialize Flask app."""
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import request, render_template, make_response, url_for
+from datetime import datetime as dt
+from flask import current_app as app
+from .models import db, User
 
 
-app = Flask(__name__, instance_relative_config=False)
-app.config.from_object("config.Config")
+@app.route('/', methods=['GET'])
+def user_records():
+    """Create a user via query string parameters."""
+    username = request.args.get('user')
+    email = request.args.get('email')
+    if username and email:
+        new_user = User(
+            username=username,
+            email=email,
+            created=dt.now(),
+            bio="In West Philadelphia born and raised, \
+            on the playground is where I spent most of my days",
+            admin=False
+        )
+        db.session.add(new_user)  # Adds new User record to database
+        db.session.commit()  # Commits all changes
+    return make_response(f"{new_user} successfully created!")
 
+@app.route('/', methods=['GET'])
+def user_records():
+    """Create a user via query string parameters."""
+    username = request.args.get('user')
+    email = request.args.get('email')
+    if username and email:
+        existing_user = User.query.filter(
+            User.username == username or User.email == email
+        ).first()
+        if existing_user:
+            return make_response(
+                f'{username} ({email}) already created!'
+            )
+        new_user = User(
+            username=username,
+            email=email,
+            created=dt.now(),
+            bio="In West Philadelphia born and raised, \
+            on the playground is where I spent most of my days",
+            admin=False
+        )  # Create an instance of the User class
+        db.session.add(new_user)  # Adds new User record to database
+        db.session.commit()  # Commits all changes
+        redirect(url_for('user_records'))
+    return render_template(
+        'users.jinja2',
+        users=User.query.all(),
+        title="Show Users"
+    )
 
-db = SQLAlchemy()
-db.init_app(app)
-db.create_all()  # Create database tables for our data models
-
-
-@app.route("/", methods=["GET"])
-def index():
-    return "hello"
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5008, debug=True)
+@app.route('/', methods=['GET'])
+def create_user():
+    """Create a user."""
+    ...
+    return render_template(
+        'users.jinja2',
+        users=User.query.all(),
+        title="Show Users"
+    )
